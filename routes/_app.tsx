6a2e41f10f37727cfe8b3379cb093dce88c34f10
Audrow Nash <114802901+paudrow@users.noreply.github.com>
2024-08-25 10:35:21 -0500

    Add share image (#9)
    * Attempt to add share image
    
    * Add more Twitter meta data
    
    * Add X share image
    
    * Fix share image
    
    * Update X share image
    
    * Set the share image for posts in app
    
    * Move constants

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,15 +1,37 @@
 import { type PageProps } from "$fresh/server.ts";
+import { Post } from "../utils/posts.ts";
+import {
+  DEFAULT_DESCRIPTION,
+  DEFAULT_SHARE_IMAGE,
+  DEFAULT_SHARE_X_IMAGE,
+  THEME_COLOR,
+} from "../constants.ts";
 
-export default function App({ Component }: PageProps) {
+export default function App(pageProps: PageProps & { data?: unknown }) {
+  const { data, url, Component } = pageProps;
+
+  const domain = url.origin;
+  const post = data?.post as Post | undefined;
+  const shareImage = post?.cover.rect
+    ? `${domain}${post.cover.rect}`
+    : `${domain}${DEFAULT_SHARE_IMAGE}`;
+  const shareXImage = post?.cover.rect
+    ? `${domain}${post.cover.rect}`
+    : `${domain}${DEFAULT_SHARE_X_IMAGE}`;
+  const title = post?.title
+    ? `${data.post.title} - Audrow Nash Podcast`
+    : "Audrow Nash Podcast";
+  const description = data?.post?.snippet || DEFAULT_DESCRIPTION;
+
   return (
     <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Audrow Nash Podcast - Exploring the Frontiers of Robotics</title>
+        <title>{title}</title>
         <meta
           name="description"
-          content="Join Audrow Nash for in-depth conversations with robotics experts, exploring cutting-edge technologies and the future of intelligent machines."
+          content={description}
         />
         <meta
           name="keywords"
@@ -18,16 +40,27 @@ export default function App({ Component }: PageProps) {
         <meta name="author" content="Audrow Nash" />
         <meta
           property="og:title"
-          content="Audrow Nash Podcast - Exploring the Frontiers of Robotics"
+          content={title}
         />
         <meta
           property="og:description"
-          content="Join Audrow Nash for in-depth conversations with robotics experts, exploring cutting-edge technologies and the future of intelligent machines."
+          content={description}
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://audrownashpodcast.com" />
-        <link rel="canonical" href="https://audrownashpodcast.com" />
-        <meta name="theme-color" content="#3A7E99" />
+        <meta property="og:url" content={domain} />
+        <meta property="og:image" content={shareImage} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content={title}
+        />
+        <meta name="twitter:image" content={shareXImage} />
+        <meta
+          name="twitter:description"
+          content={description}
+        />
+        <link rel="canonical" href={domain} />
+        <meta name="theme-color" content={THEME_COLOR} />
         <style>
           {`
           @font-face {
